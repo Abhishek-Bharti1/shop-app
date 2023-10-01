d8dd0ed0d8f8649743e5f28fe212be04d43ce353
Abhishek Bharti <abhishekbharti91550@gmail.com>
2023-10-02 01:52:39 +0530

    login register

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-// import Home from "./pages/Home";
+import Cart from "./pages/Cart";
 
-import ProductList from "./pages/ProductList";
 
 
 function App() {
-  return <ProductList/>
+  return <Cart/>
 }
 
 export default App;
